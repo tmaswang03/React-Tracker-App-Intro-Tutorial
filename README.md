a5db34917474541a45ec3f6a72841de4c67a21a0
tmaswang69 <60526513+tmaswang69@users.noreply.github.com>
2021-08-19 14:18:05 -0400

    Code taken from my local files (C:)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # React Tracker App Intro Tutorial
  My own creation of a task tracking app made with react 
+
+
+
+I did not create this app by myself. This project was created following the tutorial from Traversy Media (https://www.youtube.com/watch?v=w7ejDZ8SWv8) . The original github repository can be found here : https://github.com/bradtraversy/react-crash-2021 :D. 
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import {useState} from 'react'
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: true
+    }, 
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30 pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: false
+    }
+  ])
+  // add tasks 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1)
+    const newTask = {id, ... task}
+    setTasks([...tasks, newTask])
+  }
+
+  // delete Tasks 
+  const deleteTask = (id) => {
+    setTasks (tasks.filter((task) => task.id != id))
+  }
+
+  // toggle Reminder 
+  const toggleReminder = (id) => {
+    setTasks (tasks.map((task) => task.id == id ? {...task, reminder: !task.reminder}: task)) 
+  }
+
+  return (
+    <div className="container">
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/> : 'No Tasks to Show'}
+    </div>
+  );
+} 
+
+export default App;
